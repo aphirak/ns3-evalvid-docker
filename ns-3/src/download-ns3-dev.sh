cdf63df89a53a5f269a22022a19f6fa0116b5882
Aphirak JANSANG <aphirak.j@ku.ac.th>
2020-06-08 23:21:14 +0700

    adding evalvid ns3 module to be included in docker image

--- ns-3/src/download-ns3-dev.sh
+++ ns-3/src/download-ns3-dev.sh
@@ -3,11 +3,16 @@ ROOTDIR=/home/ns3
 
 
 cd ${ROOTDIR}
+#download  evalvid ns3 module
+git clone https://gitlab.com/gercom/evalvid-ns3
+
 #git clone https://gitlab.com/nsnam/ns-3-allinone
 git clone https://gitlab.com/nsnam/ns-3-allinone.git/
 
 cd ns-3-allinone
 ./download.py
 
+cd ns-3-dev/src/
+cp -R ${ROOTDIR}/evalvid-ns3 .
+mv evalvid-ns3 evalvid
 #./build.py --enable-examples --enable-tests
-
